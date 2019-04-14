14fcd740936ebad30343ef502e169ea3f862fda9
Baran Barut <bonfire.hf@gmail.com>
2019-04-14 02:31:03 -0400

    Reformat PSVForm.cs

--- PSV/PSVForm.cs
+++ PSV/PSVForm.cs
@@ -6,82 +6,82 @@ using System.Drawing;
 
 namespace PSV
 {
-	public partial class PSVForm : Form
-	{
-		public PSVForm()
-		{
-			InitializeComponent();
-		}
+    public partial class PSVForm : Form
+    {
+        public PSVForm()
+        {
+            InitializeComponent();
+        }
 
-		public bool IsGitURLValid()
-		{
-			try
-			{
-				System.Collections.Generic.IEnumerable<Reference> references = Repository.ListRemoteReferences(projectURLTextBox.Text);
-				return true;
-			}
-			catch (Exception)
-			{
-				return false;
-			}
-		}
+        public bool IsGitURLValid()
+        {
+            try
+            {
+                System.Collections.Generic.IEnumerable<Reference> references = Repository.ListRemoteReferences(projectURLTextBox.Text);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
 
-		public bool IsPathValid()
-		{
-			string[] files = Directory.GetFiles(pathToCloneTextBox.Text);
-			return files.Length == 0 ? true : false;
-		}
+        public bool IsPathValid()
+        {
+            string[] files = Directory.GetFiles(pathToCloneTextBox.Text);
+            return files.Length == 0 ? true : false;
+        }
 
-		private void OpenProjectButton_Click(object sender, EventArgs e)
-		{
-			if (!IsGitURLValid())
-			{
-				MessageBox.Show("Git URL is not valid", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				return;
-			}
+        private void OpenProjectButton_Click(object sender, EventArgs e)
+        {
+            if (!IsGitURLValid())
+            {
+                MessageBox.Show("Git URL is not valid", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
 
-			if (!IsPathValid())
-			{
-				MessageBox.Show("Folder in path is not empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				return;
-			}
+            if (!IsPathValid())
+            {
+                MessageBox.Show("Folder in path is not empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
 
-			Repository.Clone(projectURLTextBox.Text, pathToCloneTextBox.Text);
+            Repository.Clone(projectURLTextBox.Text, pathToCloneTextBox.Text);
 
-			var rootDirectoryInfo = new DirectoryInfo(pathToCloneTextBox.Text);
-			scanTree.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
+            var rootDirectoryInfo = new DirectoryInfo(pathToCloneTextBox.Text);
+            scanTree.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
 
-		}
+        }
 
-		private static TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
-		{
-			var directoryNode = new TreeNode(directoryInfo.Name);
-			foreach (var directory in directoryInfo.GetDirectories())
-				directoryNode.Nodes.Add(CreateDirectoryNode(directory));
-			foreach (var file in directoryInfo.GetFiles())
-				directoryNode.Nodes.Add(new TreeNode(file.Name));
-			return directoryNode;
-		}
+        private static TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeNode(directoryInfo.Name);
+            foreach (var directory in directoryInfo.GetDirectories())
+                directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+            foreach (var file in directoryInfo.GetFiles())
+                directoryNode.Nodes.Add(new TreeNode(file.Name));
+            return directoryNode;
+        }
 
-		private void OpenFolderButton_Click(object sender, EventArgs e)
-		{
-			DialogResult openFolderDialogResult = folderBrowserDialog.ShowDialog();
-			if(openFolderDialogResult == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
-			{
-				pathToCloneTextBox.Text = folderBrowserDialog.SelectedPath;
-			}
-		}
+        private void OpenFolderButton_Click(object sender, EventArgs e)
+        {
+            DialogResult openFolderDialogResult = folderBrowserDialog.ShowDialog();
+            if (openFolderDialogResult == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+            {
+                pathToCloneTextBox.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
 
-		private void TestURLButton_Click(object sender, EventArgs e)
-		{
-			if (IsGitURLValid())
-			{
-				testURLButton.ForeColor = Color.Green;
-			}
-			else
-			{
-				testURLButton.ForeColor = Color.Red;
-			}
-		}
-	}
+        private void TestURLButton_Click(object sender, EventArgs e)
+        {
+            if (IsGitURLValid())
+            {
+                testURLButton.ForeColor = Color.Green;
+            }
+            else
+            {
+                testURLButton.ForeColor = Color.Red;
+            }
+        }
+    }
 }
