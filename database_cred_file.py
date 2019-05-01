8c18703dc9f5753481b971adc6afbd129c30048a
Kyle Reid <40274419+kyReid@users.noreply.github.com>
2019-05-01 14:51:28 -0400

    Update database_cred_file.py

--- database_cred_file.py
+++ database_cred_file.py
@@ -1,4 +1,4 @@
-username = 'kyle'
-host_name = '157.230.57.38'
-psswrd = '&9h+qn4?pu2D7E6JeTeDp-87TKYjvrd+VRU#4jQgGSzxAju=Ld?ctmZc6Qw#N6Wy'
-database_name = 'api'
+username = 'username'
+host_name = 'host name'
+psswrd = 'password'
+database_name = 'database name'
