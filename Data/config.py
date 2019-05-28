e56ab7fdc6ab26c0b028a393eb88a7308f11a537
Kyle Reid <40274419+kyReid@users.noreply.github.com>
2019-05-28 16:29:07 -0400

    several file changes
    created config file
    deleted database connection file
    editted main file
    redid pipfile
    got a new access token

---
+++ Data/config.py
@@ -0,0 +1,4 @@
+user = 'kyle'
+host = '157.230.57.38'
+password = '&9h+qn4?pu2D7E6JeTeDp-87TKYjvrd+VRU#4jQgGSzxAju=Ld?ctmZc6Qw#N6Wy'
+database = 'api'
--- Data/main.py
+++ Data/main.py
@@ -1,15 +1,33 @@
-from database_connection import cursor
-from database_connection import conn
+from token_access import git_access
+from config import database
+from config import host
+from config import password
+from config import user
 from github import Github
-from token_file import access_token
 from git import Repo
-import os
-import time
+import mysql.connector
+from mysql.connector import errorcode
+
+try:
+    conn = mysql.connector.connect(user=user, host=host,
+                                   password=password,
+                                   database=database)
+    cursor = conn.cursor()
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+    conn.close()
 
 # My personal github API token, must have to access data
-git = Github(access_token)
+git = Github(git_access)
 
-# get the name of the online repo, and the directory on the local computer of same repo
+# get the name of the online repo, and the directory on the local computer of
+# same repo
 # using two different libraries getting data different ways
 repo_name = input('Name of Github Repo: ')
 github_repo = git.get_repo(repo_name, lazy=False)
@@ -37,28 +55,32 @@ def repo_data():
     watchers = github_repo.watchers_count
     repo_creation_date = str(github_repo.created_at)
 
-    sql = """INSERT INTO repo (repo_name, repo_initial_creation, stargazers, watchers, subscribers, contributors,
-    branches, assignees, labels, forks, pulls, events, network, open_issues) VALUES (%s,
-    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-
-    data = [repo_name, repo_creation_date, stargazer, watchers, subscribers, contributors, branches,
-            assignees, labels, forks, pulls, events, network_count, issues]
+    # TODO get mysql data connection
+    # sql = """INSERT INTO repo (repo_name, repo_initial_creation, stargazers,
+    # watchers, subscribers, contributors,
+    # branches, assignees, labels, forks, pulls, events, network, open_issues)
+    # VALUES (%s,
+    # %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+    #
+    # data = [repo_name, repo_creation_date, stargazer, watchers, subscribers,
+    #         contributors, branches,
+    #         assignees, labels, forks, pulls, events, network_count, issues]
 
     # TODO Determine why data wont execute
-    try:
-        print("trying")
-        # store data in sql database
-        cursor.execute(sql, data)
-        print("data executed")
-        # make sure data is committed to database
-        conn.commit()
-        print("data commited")
-        # close connections
-        cursor.close()
-        conn.close()
-        print("data added successfully")
-    except Exception:
-        print("data not added")
+    # try:
+    #     print("trying")
+    #     # store data in sql database
+    #     cursor.execute(sql, data)
+    #     print("data executed")
+    #     # make sure data is committed to database
+    #     conn.commit()
+    #     print("data committed")
+    #     # close connections
+    #     cursor.close()
+    #     conn.close()
+    #     print("data added successfully")
+    # except Exception:
+    #     print("data not added")
 
 
 def file_data(repo):
@@ -73,12 +95,10 @@ def file_data(repo):
     # with all the commits, go through and pull data
     commits = list(repo.iter_commits('master'))[:1]
     for commit in commits:
-        hexsha = commit.hexsha
         committed_datetime = commit.committed_datetime
         commit_size = commit.size
         commit_stats = commit.stats.files
 
-        print("hexsha: ", hexsha)
         print("committed datetime:", str(committed_datetime))
         print("commit size:", commit_size)
         # iterations through commit stats and parses dictionaries into
@@ -89,31 +109,6 @@ def file_data(repo):
             print(file_path)
             for type_of_change, change_value in value.items():
                 print(type_of_change, change_value)
-
-    print("-" * 80)
-
-    # # list of all the files in project directory and subdirectories
-    # files = []
-    # # r = root, d = directory, f = files
-    # for r, d, f in os.walk('C:\openssl'):
-    #     for file in f:
-    #         if ".z" not in file:
-    #             files.append(os.path.join(r,file))
-    # # print file count
-    # total = 0
-    # for f in files:
-    #     total += len(f)
-    #
-    # print("total file count:", total)
-    # #==========================================================
-    # files = folders = 0
-    #
-    # for _, dirnames, filenames in os.walk("C:\openssl"):
-    #     # ^ this idiom means "we won't be using this value"
-    #         files += len(filenames)
-    #         folders += len(dirnames)
-    #
-    # print("{:,} files, {:,} folders".format(files, folders))
 
     # C:\Users\bbkyl\Dropbox\School\Senior-Design-Project\test\openssl
 
