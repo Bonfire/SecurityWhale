9096e1c57cdc98deb48710b3f49ce59c86733245
Kyle Reid <kyreid14@gmail.com>
2019-10-17 15:00:22 -0400

    fixed security issue *cough*

--- Data/app/config.py
+++ Data/app/config.py
@@ -3,9 +3,9 @@
 
 # all the important variables for gaining access to the github API and the database
 # are stored here for ease of access
-user = 'kyle'
-host = '157.230.57.38'
-password = '&9h+qn4?pu2D7E6JeTeDp-87TKYjvrd+VRU#4jQgGSzxAju=Ld?ctmZc6Qw#N6Wy'
-database = 'api'
+user = 'username'
+host = 'host'
+password = 'password'
+database = 'database'
 
-git_access = '33465c9b4cbcea2a21e9f9ba60305adc355e8483'
+git_access = 'access token'
