8d3a7780341764b36c771ad81ed60f9bb5715bdf
Kyle Reid <kyreid14@gmail.com>
2019-10-18 13:10:29 -0400

    beginning process of application integration
    check TODOs for detail

--- Data/app/main.py
+++ Data/app/main.py
@@ -1,27 +1,52 @@
 from SecWhale import *
-from time import time
 
-if __name__ == "__main__":
-
+def main():
+    
+    # TODO: create either and if elif statement to run script with application or....
+    # this is for training
     try:
-
         start_time = time.time()
-
         git = access_github()
+        
+        #  Stored parsed csv file for repo cloning and parsing
+        temp = get_repos()
+    
+        # each repo from list is cloned and parsed, as long as repo exists then obliterates it
+        for name in temp:
+            # used for collecting data for repo table
+            github_repo = git.get_repo(name[0], lazy=False)
+            repo_dir = clone_repo(name[0])
+            # used in collecitng data in both tables
+            repo = Repo(str(repo_dir))
 
-        with open('../docs/Github_Testing.txt', 'r') as repo_info:
-            for row in repo_info.readlines():
-                repo_name, repo_dir = row.split(' ')
-                github_repo = git.get_repo(repo_name, lazy=False)
-                repo = Repo(repo_dir.rstrip())
+            # here we check if we can access the actual repository object
+            if not repo.bare:
+                repo_database(repository_data(github_repo, repo, name[0], repo_dir))
 
-                if not repo.bare:
-                    repo_database_connector(repository_data(github_repo, repo,
-                                                            repo_name, repo_dir.rstrip()))
-                    log_data(repo)
-                else:
-                    print('Could not load repository at {} :'.format(repo_dir))
+                # remove repo name from list so we only have the commit hashes to look at
+                name.remove(name[0])
+                
+                # data parsing
+                for hash_commits in name:
+                    dirty_data(repo, hash_commits)
+                    clean_data(repo, hash_commits)
+
+            else:
+                print('Could not load repository at {} :'.format(repo_dir))
+                
+            # deletes entire cloned repo before cloning the next to save memory on local machines
+            obliterate(repo_dir)
 
         print('Script Complete|Runtime: {} Seconds'.format(time.time() - start_time))
     except Exception as e:
         print("Could not check if repo existed", e)
+        
+    # TODO: implement code that will take data from application
+    try:
+        pass
+    except Exception as e:
+        print("Could not check if repo existed", e)
+
+        
+if __name__ == "__main__":
+    main()
