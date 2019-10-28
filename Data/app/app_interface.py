892e99e0d4271d7e5adbc212ee4c76e31851f164
TomSerrano <39391571+TomSerrano@users.noreply.github.com>
2019-10-28 19:07:24 -0400

    Should correctly collect & organize data

--- Data/app/app_interface.py
+++ Data/app/app_interface.py
@@ -1,11 +1,17 @@
-import sys
+'''
+Author: Thomas Serrano
+
+'''
+import application		#Application stuff
+import conn					#ML stuff
+import sys					#For command-line arguments
+from git import Repo
 from github import Github
-import application.py
+from os import walk			#For parsing through directories
 
 '''
-Interfaces the desktop application with the data parsing layer
-'''
 
+'''
 def app_interface():
 	#Get command-line arguments
 	username = sys.argv[1]
@@ -13,21 +19,44 @@ def app_interface():
 	repo_name = sys.argv[3]
 	clone_dir = sys.argv[4]
 	
-	#Do they want to add it to the DB?
-	add_signal = sys.argv[5]
-	
 	print("Starting interface...")
-	print("Arguments:\n" + username + " / " + password + " / " + repo_name + " / " + clone_dir + " / " + add_signal)
+	print("Arguments:\n" + username + " / " + password + " / " + repo_name + " / " + clone_dir)
 	
 	#Get Github access
 	git = Github(username, password)
+
+    #Grabs repo object for repo table data (collecting)
+	github_repo = git.get_repo(repo_name, lazy=False)
+	repo = Repo(clone_dir)
 	
-	#Retrieve repo & github data
-	repo, git_data = repo_get(repo_name, git, clone_dir, add_signal)
+	#Retrieve repository data
+	repo_data = repository_data(github_repo, repo, repo_name, clone_dir)
 	
-	#Go through files in the repo...
+	#Retrieve all filenames for all directories
+	files = []
+	for (dirpath, dirnames, filenames) in walk(path):        
+		for filename in [f for f in filenames]:
+			files.append(filename)
+			break
+	
+	avgs = get_averages(files, hash, repo)
+	
+	#Collect data together into a single list
+	final_data = []
+	for a in avgs:
+		final_data.append(a + repo_data)
+	
+	#Predict and print results data to std out
+	'''
+	fin_csv
+	i = 0
+	results = predict(final_data)
+	for r in results:
+		fin_csv.append(files[i] + "," + str(r))
+		
+	print(fin_csv)
+	'''
 
 #Main - delete later?
 if __name__ == "__main__":
 	app_interface()
-#app_interface()
