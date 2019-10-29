2d7fec3280f3af0e2d350926ef2ef622f17695db
TomSerrano <39391571+TomSerrano@users.noreply.github.com>
2019-10-28 22:06:09 -0400

    Should now correctly collate data

--- Data/app/app_interface.py
+++ Data/app/app_interface.py
@@ -1,52 +1,54 @@
 '''
 Author: Thomas Serrano
-
+Last Updated: OCT-28-2019
 '''
-import application		#Application stuff
-import conn					#ML stuff
+from application import *	#Application stuff
+#import conn					#ML stuff
 import sys					#For command-line arguments
 from git import Repo
 from github import Github
 from os import walk			#For parsing through directories
 
 '''
-
+Provides an interface to connect the application with the machine learning model
 '''
 def app_interface():
 	#Get command-line arguments
 	username = sys.argv[1]
 	password = sys.argv[2]
 	repo_name = sys.argv[3]
-	clone_dir = sys.argv[4]
+	repo_dir = sys.argv[4]
 	
 	print("Starting interface...")
-	print("Arguments:\n" + username + " / " + password + " / " + repo_name + " / " + clone_dir)
+	#print("Arguments:\n" + username + " / " + password + " / " + repo_name + " / " + repo_dir)
 	
 	#Get Github access
 	git = Github(username, password)
 
-    #Grabs repo object for repo table data (collecting)
+	#Grabs repo object for data collection
 	github_repo = git.get_repo(repo_name, lazy=False)
-	repo = Repo(clone_dir)
+	repo = Repo(repo_dir)
+	
+	#print(repo.head.commit.hexsha)
 	
 	#Retrieve repository data
-	repo_data = repository_data(github_repo, repo, repo_name, clone_dir)
+	repo_data = list(repository_data(github_repo, repo, repo_name, repo_dir))
 	
 	#Retrieve all filenames for all directories
 	files = []
-	for (dirpath, dirnames, filenames) in walk(path):        
+	for (dirpath, dirnames, filenames) in walk(repo_dir):        
 		for filename in [f for f in filenames]:
 			files.append(filename)
-			break
 	
-	avgs = get_averages(files, hash, repo)
+	#Get a list of data points for each file
+	avgs = get_averages(files, repo.head.commit.hexsha, repo)
 	
 	#Collect data together into a single list
 	final_data = []
 	for a in avgs:
 		final_data.append(a + repo_data)
 	
-	#Predict and print results data to std out
+	#TODO: Predict and print results data to std out
 	'''
 	fin_csv
 	i = 0
